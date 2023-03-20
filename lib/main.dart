d9a0be5ba1d3bf3b7b3d3a3fbca01eb6478f44f9
lincoln5702 <basnetlincoln541@gmail.com>
2023-03-20 23:21:30 +0545

    link

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,38 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:ui' show lerpDouble;
+import 'package:get/get.dart';
 
-void main(){
+import 'package:minor/controllers/coin_controller.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,//use MaterialApp() widget like this
+        debugShowCheckedModeBanner: false, //use MaterialApp() widget like this
         home: SplashScreen() //create new widget class for this 'home' to
-      // escape 'No MediaQuery widget found' error
-    );
+        // escape 'No MediaQuery widget found' error
+        );
   }
 }
 
 //create new class for "home" property of MaterialApp()
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final CoinController coinController = Get.put(CoinController());
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-
+      // backgroundColor: Colors.black,
 
       //theme rakhne ki na rakhne bhanera confused
 
@@ -52,56 +58,153 @@ class HomeScreen extends StatelessWidget {
           ),
       textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.purple)),
       ),*/
-        appBar: AppBar(
-          elevation: 0.0,
-          title: const Text('Prophecy'),
-          backgroundColor: Colors.black,
-          //theme: ThemeData(fontFamily: 'Bebas Neue Regular'),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications),
-            ),
-          ],
-
+      appBar: AppBar(
+        elevation: 0.0,
+        title: const Text('Prophecy'),
+        backgroundColor: Colors.black,
+        //theme: ThemeData(fontFamily: 'Bebas Neue Regular'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications),
+          ),
+        ],
+      ),
+      // body:Container(
+      //   child: SingleChildScrollView(
+      //
+      //   ),
+      // ),
+      body: Container(
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(children: [
+            Obx(
+              () => coinController.isLoading.value
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: 100,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 60,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      width: 60,
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                          color: Colors.blueAccent,
+                                          borderRadius:
+                                              BorderRadius.circular(15)),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Image.network(coinController.coinslist[index].image),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(coinController.coinslist[index].name,
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600)),
+                                        Text("${coinController.coinslist[index].priceChangePercentage24H}%",
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600)),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text("\$ ${coinController.coinslist[index].currentPrice}",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600)),
+                                    Text(coinController.coinslist[index].symbol,
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600)),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+            )
+          ]),
         ),
-        bottomNavigationBar : Container(
-          color: Colors.black,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 20),
-            child: GNav(
-              backgroundColor: Colors.black,
-              color: Colors.white,
-              activeColor: Colors.white,
-              tabBackgroundColor: Colors.grey.shade800,
-              gap: 8,
-              //onTabChange: ,
-              padding: const EdgeInsets.all(16),
+        height: 800,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.grey,
+          //color: Color(0xFF81D4FA),
+          // borderRadius:BorderRadius.only(
+          //   topLeft: Radius.circular(30),
+          //   topRight: Radius.circular(30),
+        ),
+      ),
 
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+          child: GNav(
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            gap: 8,
+            //onTabChange: ,
+            padding: const EdgeInsets.all(16),
 
-              tabs: const [
-                GButton(
-                  icon: Icons.home,
-                  text: 'Home',
-                ),
-                GButton(
-                  icon: Icons.favorite_border,
-                  text: 'Wishlist',
-                ),
-                GButton(
-                  icon: Icons.search,
-                  text: 'Search',
-                ),
-                GButton(
-                  icon: Icons.newspaper,
-                  text: 'News',
-                ),
-              ],
-    ),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.favorite_border,
+                text: 'Wishlist',
+              ),
+              GButton(
+                icon: Icons.search,
+                text: 'Search',
+              ),
+              GButton(
+                icon: Icons.newspaper,
+                text: 'News',
+              ),
+            ],
           ),
-          ),
-        /*bottomNavigationBar: BottomNavigationBar(
+        ),
+      ),
+      /*bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.newspaper),
@@ -118,56 +221,43 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),*/
-        body: Container(
-          height: 800,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            color: Colors.grey,
-            //color: Color(0xFF81D4FA),
-            borderRadius:BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-          ),
-        ),
-        drawer: Drawer(
+
+      drawer: Drawer(
           child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
           const DrawerHeader(
-          decoration: BoxDecoration(
-          color: Colors.blue,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
           ),
-          child: Text('Drawer Header'),
-        ),
-        ListTile(
-          title: const Text('Item 1'),
-          onTap: () {
-            // Update the state of the app
-            // ...
-            // Then close the drawer
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-        title: const Text('Item 2'),
-    onTap: () {
-    // Update the state of the app
-    // ...
-    // Then close the drawer
-    Navigator.pop(context);
-    },
-        ),
-    ],
-    )
-        ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )),
     );
   }
 }
 
-
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
@@ -176,15 +266,14 @@ class SplashScreen extends StatefulWidget{
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds:3)).then((value){
-      Navigator.of(context).pushReplacement(
-          CupertinoPageRoute(builder:(ctx) => const HomeScreen()));
-
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      Navigator.of(context)
+          .pushReplacement(CupertinoPageRoute(builder: (ctx) => HomeScreen()));
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -193,17 +282,17 @@ class _SplashScreenState extends State<SplashScreen> {
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const[
+          children: const [
             Image(
               image: AssetImage("assets/images/1.png"),
               width: 300,
             ),
             SizedBox(
-            height : 50,
+              height: 50,
             ),
             SpinKitFadingCircle(
-            color: Colors.black,
-            size: 50.0,
+              color: Colors.black,
+              size: 50.0,
             ),
           ],
         ),
@@ -211,5 +300,3 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-
-
